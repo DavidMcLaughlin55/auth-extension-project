4849c652594445812a06451bb9fc6c9e44b5a59d
DavidMcLaughlin55 <49570547+DavidMcLaughlin55@users.noreply.github.com>
2023-09-19 21:09:27 -0400

    pop up can be opened in chrome

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>Sign In</h2>
+      <form>
+        <label htmlFor="emailAddress">Email Address</label>
+        <input id="emailAddress" name="emailAddress" type="email" placeholder="required"></input>
+        <label htmlFor="password">Password</label>
+        <input id="password" name="password" type="password" placeholder="required"></input>
+        <button className="button" type="submit">Sign In</button>
+      </form>
     </div>
   );
 }
